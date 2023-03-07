dee5e1573552c4e5fe9612d20115e05f6e57030d
SAZZAD1Q2 <kurulia115@gmail.com>
2023-03-08 00:00:55 +0600

    index.html file modified

--- navbar.js
+++ navbar.js
@@ -1,9 +1,32 @@
-let btn = document.querySelector('#btn');
-btn.addEventListener('click', function(event) {
-    console.log('The button was clicked!');
-    // event.stopPropagation();
-});
 
-document.body.addEventListener('click',function(event) {
-    console.log('The body was clicked!');
-});
+const mobileMenu = document.querySelectorAll('.mobile-menu-canvas li');
+const mobileMenuCanvas = document.querySelector('.mobile-menu-canvas');
+
+for (let i = 0; i < mobileMenu.length; i += 1) {
+  mobileMenu[i].addEventListener('click', () => {
+    mobileMenuCanvas.classList.toggle('show');
+    mobileMenuIcons[0].classList.toggle('hide');
+  });
+}
+
+for (let i = 0; i < mobileMenuIcons.length; i += 1) {
+  mobileMenuIcons[i].addEventListener('click', () => {
+    mobileMenuCanvas.classList.toggle('show');
+  });
+}
+
+// let btn = document.querySelector('#btn');
+// btn.addEventListener('click', function(event) {
+//     console.log('The button was clicked!');
+//     // event.stopPropagation();
+// });
+
+// document.body.addEventListener('click',function(event) {
+//     console.log('The body was clicked!');
+// });
+
+// let monileManu = document.querySelector(".mobile_manu");
+
+// mobileManu.addEventListener('click')
+
+/* sei */
