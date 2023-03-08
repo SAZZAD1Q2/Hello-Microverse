af8102f10f1ad80bda3b2de37f1bf3ffcf48a2bf
SAZZAD1Q2 <kurulia115@gmail.com>
2023-03-08 11:12:18 +0600

    Javascript file update

--- navbar.js
+++ navbar.js
@@ -1,19 +1,18 @@
-
 const mobileMenu = document.querySelectorAll('.mobile-menu-canvas li');
 const mobileMenuCanvas = document.querySelector('.mobile-menu-canvas');
 
 for (let i = 0; i < mobileMenu.length; i += 1) {
   mobileMenu[i].addEventListener('click', () => {
     mobileMenuCanvas.classList.toggle('show');
-    mobileMenuIcons[0].classList.toggle('hide');
+    // mobileMenuIcons[0].classList.toggle('hide');
   });
 }
 
-for (let i = 0; i < mobileMenuIcons.length; i += 1) {
-  mobileMenuIcons[i].addEventListener('click', () => {
-    mobileMenuCanvas.classList.toggle('show');
-  });
-}
+// for (let i = 0; i < mobileMenuIcons.length; i += 1) {
+//   mobileMenuIcons[i].addEventListener('click', () => {
+//     mobileMenuCanvas.classList.toggle('show');
+//   });
+// }
 
 // let btn = document.querySelector('#btn');
 // btn.addEventListener('click', function(event) {
